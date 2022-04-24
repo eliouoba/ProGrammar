e5926b90d4ce79653b3fe3a83a16682b043b22fd
josiahhsu <77859877+josiahhsu@users.noreply.github.com>
2022-04-24 16:51:36 -0500

    linked custom lessons to database

--- src/scripts/custom-lesson-driver.js
+++ src/scripts/custom-lesson-driver.js
@@ -1,13 +1,37 @@
 //Josiah Hsu
 
+
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getDatabase, ref, get, set, update} from "firebase/database";
+
 import Input from "./Input-Class.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC8TjMHSCAqxaqlIW2MNdbWWLp_vyWUBHA",
+    authDomain: "programmar-d33e8.firebaseapp.com",
+    databaseURL: "https://programmar-d33e8-default-rtdb.firebaseio.com",
+    projectId: "programmar-d33e8",
+    storageBucket: "programmar-d33e8.appspot.com",
+    messagingSenderId: "1017841820021",
+    appId: "1:1017841820021:web:943e79503ad7292bb6b33c",
+    measurementId: "G-6QTSB873J8"
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const auth = getAuth(app);
 
 //quick element references
 const resetButton = document.getElementById("reset");
 const toType = document.getElementById("toType");
+const fileSelect = document.getElementById('fileSelect');
 const uploader = document.getElementById('uploader');
-uploader.oninput=setText;
+const downloader = document.getElementById('downloader');
+fileSelect.oninput=loadTextFromFile;
 resetButton.onclick=reset;
+uploader.onclick=uploadLesson;
+downloader.onclick=loadTextFromDB;
 
 let interval, start, end; //timer
 let typer = new Input();
@@ -21,11 +45,28 @@ document.getElementById("help").innerHTML += allowedMsg;
 
 const reader = new FileReader();
 
-function setText(){
-    console.log(uploader);
-    let selectedFile = uploader.files[0];
-
+function uploadLesson(){
+    let selectedFile = fileSelect.files[0];
     if(selectedFile == null) return;
+    const user = auth.currentUser.uid;
+    const lessonsReference = ref(database, `users/${user}/lessons`);
+    get(lessonsReference).then((snapshot) => {
+        let newLesson = typer.toTypeText;
+        let name = prompt("Enter a name for your lesson.");
+        if(name == null || name == '') return;
+        if(snapshot.hasChild(name) && !confirm(`Lesson '${name}' already exists. Overwrite?`))
+            return;
+        set(ref(database, `users/${user}/lessons/${name}`), newLesson);
+    }).catch((error) => {
+        console.error(error);
+    });
+}
+
+function loadTextFromFile(){
+    let selectedFile = fileSelect.files[0];
+    if(selectedFile == null) return;
+
+    uploader.disabled = false;
 
     //check file extension to see if selected file is valid
     let extension = selectedFile.name.split('.').pop();
@@ -40,9 +81,33 @@ function setText(){
         typer.init();
         document.addEventListener("keydown", startLesson);
         resetButton.hidden = false;
-        uploader.blur();
+        fileSelect.blur();
         reader.removeEventListener("load", this);
     }, false);
+}
+
+function loadTextFromDB(){
+    let name = prompt("What is the name of the lesson you previously uploaded?")
+    if(name == null) return;
+    const user = auth.currentUser.uid;
+    const lessonsReference = ref(database, `users/${user}/lessons`);
+    get(lessonsReference).then((snapshot) => {
+        if(snapshot.hasChild(name))
+            setText(snapshot.child(name).val());
+        else
+            alert("Error: No such lesson exists.");
+    }).catch((error) => {
+        console.error(error);
+    });
+}
+
+function setText(text){
+    typer.toTypeText = text;
+    typer.init();
+    document.addEventListener("keydown", startLesson);
+    resetButton.hidden = false;
+    fileSelect.blur();
+    reader.removeEventListener("load", this);
 }
 
 /**
@@ -51,7 +116,7 @@ function setText(){
 function reset() {
     alert("Resetting!");
     resetButton.blur();
-    uploader.disabled = false;
+    setConfigDisabled(false);
     window.clearInterval(interval);
     document.removeEventListener("keydown", type);
     document.addEventListener("keydown", startLesson);
@@ -77,11 +142,17 @@ function startLesson(keydownEvent) {
         document.addEventListener("keydown", type);
         start = Date.now();
         interval = window.setInterval(timer, 250);
-        uploader.disabled = true;
+        setConfigDisabled(true);
         typer.input(keydownEvent.key);
     }
 }
 
+
+function setConfigDisabled(disable){
+    fileSelect.disabled = disable;
+    uploader.disabled = disable;
+    downloader.disabled = disable;
+}
 /**
  * type - records input from keyboard in tracker
  * @param {*} key the key entered
@@ -105,6 +176,6 @@ function endLesson() {
     typer.time = (end - start) / 1000;
     typer.updateWPM();
     typer.displayStats();
-    uploader.disabled = false;
+    setConfigDisabled(false);
     alert(stats.textContent);
 }
