590b1880a96d08ed0d629b2fb125ab21e23b2a3c
josiahhsu <77859877+josiahhsu@users.noreply.github.com>
2022-04-26 01:18:04 -0500

    Made custom lesson loading menu-based

--- src/scripts/custom-lesson-driver.js
+++ src/scripts/custom-lesson-driver.js
@@ -1,9 +1,8 @@
 //Josiah Hsu
 
-
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getDatabase, ref, get, set, update} from "firebase/database";
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { getDatabase, ref, get, set} from "firebase/database";
 
 import Input from "./Input-Class.js";
 
@@ -22,28 +21,61 @@ const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 const auth = getAuth(app);
 
+onAuthStateChanged(auth, initLessonList);
+
 //quick element references
 const resetButton = document.getElementById("reset");
 const toType = document.getElementById("toType");
 const fileSelect = document.getElementById('fileSelect');
-const uploader = document.getElementById('uploader');
-const downloader = document.getElementById('downloader');
+const saveButton = document.getElementById('saveButton');
+const loadMenu = document.getElementById('loadMenu');
+
 fileSelect.oninput=loadTextFromFile;
 resetButton.onclick=reset;
-uploader.onclick=saveLesson;
-downloader.onclick=loadTextFromDB;
+saveButton.onclick=saveLesson;
+loadMenu.onchange=loadTextFromDB;
 
 let interval, start, end; //timer
 let typer = new Input();
 
-let allowed = ['java', 'c', 'py']; //defines allowed files - can expand list as needed
+//defines allowed files - can expand list as needed
+let supportedFiles = ['java', 'c', 'py', 'js', "html", "css"];
 
-let allowedMsg = "Allowed file formats: ";
-for(var i = 0; i < allowed.length; i++)
-        allowedMsg += `${(i > 0?', ':'')}.${allowed[i]}`;
-document.getElementById("help").innerHTML += allowedMsg;
+let accept = '';
+for(var i = 0; i < supportedFiles.length; i++)
+        accept += `${(i > 0?', ':'')}.${supportedFiles[i]}`;
+fileSelect.accept = accept; //restricts file select to the listed files
+
+let acceptMsg = "Supported file formats: " + accept;
+document.getElementById("toType").textContent += '\n' + acceptMsg;
 
 const reader = new FileReader();
+
+/**
+ * initLessonList - retrieves list of lessons from database
+ */
+function initLessonList(){
+    const user = auth.currentUser.uid;
+    const lessonsReference = ref(database, `users/${user}/lessons`);
+    get(lessonsReference).then((snapshot) => {
+        snapshot.forEach((child)=>{
+            appendLessonList(child.key);
+        });
+    }).catch((error) => {
+        console.error(error);
+    });
+}
+
+/**
+ * appendLessonList - appends a lesson to the end of the list
+ * @param {*} name The name to add to the lesson list
+ */
+function appendLessonList(name){
+    let l = document.createElement('option');
+    l.value = name;
+    l.textContent = name;
+    loadMenu.appendChild(l);
+}
 
 /**
  * loadTextFromFile - loads a lesson from a user-provided file
@@ -51,26 +83,25 @@ const reader = new FileReader();
 function loadTextFromFile(){
     let selectedFile = fileSelect.files[0];
     if(selectedFile == null) return;
-
-    uploader.disabled = false;
-
+    saveButton.disabled = false;
+    document.getElementById("default").selected = true;
     //check file extension to see if selected file is valid
     let extension = selectedFile.name.split('.').pop();
-    if(!allowed.includes(extension)){
-        alert(`Error: Invalid input. Please use a supported file.\n${allowedMsg}`)
-        return;
+    if(supportedFiles.includes(extension)){
+        toType.textContent = reader.readAsText(selectedFile);
+        reader.addEventListener("load", reader.f = function fn(){
+            setText(reader.result);    
+            reader.removeEventListener("load", reader.f);
+        }, false);
     }
-
-    toType.textContent = reader.readAsText(selectedFile);
-    reader.addEventListener("load", reader.f = function fn(){
-        setText(reader.result);    
-        reader.removeEventListener("load", reader.f);
-    }, false);
+    else{
+        alert(`Error: Invalid input. Please use a supported file.\n${acceptMsg}`)
+    }
 }
 
 /**
  * setText - initializes the typer for this lesson text
- * @param {*} text 
+ * @param {*} text The lesson text to use for this custom lesson
  */
 function setText(text){
     typer.toTypeText = text;
@@ -87,13 +118,17 @@ function saveLesson(){
     let selectedFile = fileSelect.files[0];
     if(selectedFile == null) return;
     const user = auth.currentUser.uid;
+    
     const lessonsReference = ref(database, `users/${user}/lessons`);
     get(lessonsReference).then((snapshot) => {
         let newLesson = typer.toTypeText;
         let name = promptName("Enter a name for your lesson.");
         if(name == null) return;
-        if(snapshot.hasChild(name) && confirm(`Lesson '${name}' already exists. Overwrite?`))
-            set(ref(database, `users/${user}/lessons/${name}`), newLesson);
+        if(!snapshot.hasChild(name))
+            appendLessonList(name);
+        else if(!confirm(`Lesson '${name}' already exists. Overwrite?`))
+            return;
+        set(ref(database, `users/${user}/lessons/${name}`), newLesson);
     }).catch((error) => {
         console.error(error);
     });
@@ -103,15 +138,11 @@ function saveLesson(){
  * loadTextFromDB - loads a previously saved lesson (if it exists).
  */
 function loadTextFromDB(){
-    let name = promptName("Enter the name of a lesson you previously uploaded.");
-    if(name == null) return;
     const user = auth.currentUser.uid;
+    const name = loadMenu.value;
     const lessonsReference = ref(database, `users/${user}/lessons`);
     get(lessonsReference).then((snapshot) => {
-        if(snapshot.hasChild(name))
-            setText(snapshot.child(name).val());
-        else
-            alert("Error: No such lesson exists.");
+        setText(snapshot.child(name).val());
     }).catch((error) => {
         console.error(error);
     });
@@ -174,8 +205,8 @@ function startLesson(keydownEvent) {
  */
 function setConfigDisabled(disable){
     fileSelect.disabled = disable;
-    uploader.disabled = disable;
-    downloader.disabled = disable;
+    saveButton.disabled = disable;
+    loadMenu.disabled = disable;
 }
 /**
  * type - records input from keyboard in tracker
@@ -201,5 +232,4 @@ function endLesson() {
     typer.updateWPM();
     typer.displayStats();
     setConfigDisabled(false);
-    alert(stats.textContent);
 }
