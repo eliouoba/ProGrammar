027898acdb5d1ca755f951a0fc43f82482a4191d
graceflitsch <78052769+graceflitsch@users.noreply.github.com>
2022-04-28 11:44:33 -0500

    Added initial database connection and use

--- src/scripts/leader.js
+++ src/scripts/leader.js
@@ -1,3 +1,8 @@
+import { onAuthStateChanged } from 'firebase/auth';
+import { ref, get, set } from "firebase/database";
+import { auth, database } from './firebaseInit';
+
+
 var tabs = document.querySelectorAll(".lboard_tabs ul li");
 var WPM = document.querySelector(".WPM");
 var accuracy = document.querySelector(".accuracy");
@@ -30,3 +35,58 @@ tabs.forEach(function(tab){
 
 	})
 })
+
+function main(){
+	const onewpm = document.getElementById("1wpm");
+	const twowpm = document.getElementById("2wpm");
+	const threewpm = document.getElementById("3wpm");
+	const fourwpm = document.getElementById("4wpm");
+	const fivewpm = document.getElementById("5wpm");
+
+	if(currenttab == "WPM"){
+		popAndSort()
+	}
+}
+
+function sort(a, users){
+	{
+        var n = a.length;
+        for (var i = 0; i < n - 1; i++)
+            for (var j = 0; j < n - i - 1; j++)
+                if (a[j] > a[j + 1]) {
+                    var temp = a[j]
+					var temp2 = users[j]
+                    a[j] = a[j + 1]
+					users[j] = users[j+1]
+                    a[j + 1] = temp
+					users[j+1] = temp2
+                }
+    }
+}
+
+//populate stat array, user array, sort parallel to each other, and display to tabs
+function popAndSort(){
+	const userRef = ref(database, 'users');
+	var i = 0;
+	var j = 0;
+	var users=[];
+	var wpm = [];
+	//for each user, add their uID to the user array
+	const allUserStats = ref(database, `stats/users`);
+	get(allUserStats).then((snapshot) => {
+		snapshot.forEach((child) => {
+			users[i] = child.child(`username`).val();
+			wpm[j] = parseInt(child.child(`wpm`).val());
+			i++;
+			j++;
+		})
+	})
+	sort(wpm, users);
+
+	//display top 5
+	onewpm.innerHTML = wpm[0];
+	twowpm.innerHTML = wpm[1];
+	threewpm.innerHTML = wpm[2];
+	fourwpm.innerHTML = wpm[3];
+	fivewpm.innerHTML = wpm[4];
+}
