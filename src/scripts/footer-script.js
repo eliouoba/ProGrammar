f59595469dfd9ee8ebaea885debfddb33d45a65a
josiahhsu <77859877+josiahhsu@users.noreply.github.com>
2022-04-28 19:25:32 -0500

    fixed html lessons, updated footer

--- src/scripts/footer-script.js
+++ src/scripts/footer-script.js
@@ -5,22 +5,24 @@ const lessons = ["HelloWorld", "Integers", "BasicMath", "Strings",
 "Bubble", "Selection", "Insertion", "Merge", "Quick", "Heap", "Linear", "Binary", 
 "Intro", "Format", "Supersub", "Links", "Images", "Style", "Button", "Lists", "Table"];
 
-document.getElementById("next").addEventListener("click", () => {
-    let lesson = getNextLesson();
-    window.location = `lesson.html?lesson=${lesson}&lang=jpc`
-});
+document.getElementById("next").addEventListener("click", nextLesson);
 
-document.getElementById("last").addEventListener("click", () => {
-    let lesson = getLastLesson();
-    window.location = `lesson.html?lesson=${lesson}&lang=jpc`
-});
+document.getElementById("last").addEventListener("click", lastLesson);
 
-function getNextLesson() {
+function nextLesson() {
     let n = lessons.findIndex((element) => element == lessonFile) + 1;
-    return lessons[n % lessons.length];
+    n %= lessons.length;
+    window.location = `lesson.html?lesson=${lessons[n]}&lang=${getExt(n)}`
 }
 
-function getLastLesson() {
+function lastLesson() {
     let n = lessons.findIndex((element) => element == lessonFile) - 1;
-    return lessons[n < 0? lessons.length-1 : n];
+    if(n < 0)
+        n = lessons.length-1;
+    
+    window.location = `lesson.html?lesson=${lessons[n]}&lang=${getExt(n)}`;
+}
+
+function getExt(n){
+    return n < 18? 'jpc' : 'h';
 }
