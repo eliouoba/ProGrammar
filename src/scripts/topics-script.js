252965dbb48efcfdda2a41bf9162092e051ef83c
josiahhsu <77859877+josiahhsu@users.noreply.github.com>
2022-05-02 13:12:27 -0500

    Minor cleanup

--- src/scripts/topics-script.js
+++ src/scripts/topics-script.js
@@ -1,7 +1,6 @@
 // Sam Burk
 
 /* Param lesson: lesson code file
- * Will allow for loading different levels in the future
  */
 function selectLevel(lesson, lang) {
     let url = window.location.href;
